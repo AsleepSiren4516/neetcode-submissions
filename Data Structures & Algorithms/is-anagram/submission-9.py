bc701a8ef2e44d983be28fb7247ccfa91c6951fb
AsleepSiren4516 <bobby4096@outlook.com>
2026-09-17 20:12:08 -0600

    Add: is-anagram - submission-9

---
+++ Data Structures & Algorithms/is-anagram/submission-9.py
@@ -0,0 +1,14 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        if len(s) != len(t):
+            return False
+
+        wordS, wordT = {}, {}
+
+        for i in range(len(s)):
+            wordS[s[i]] = 1 + wordS.get(s[i], 0)
+            wordT[t[i]] = 1 + wordT.get(t[i], 0)
+        
+        if wordS == wordT:
+            return True
+        return False
